76496dd87fc3086a2fc8b031b619610d4419540a
Stanislav <stanislavasilevich@gmail.com>
2025-02-26 21:16:30 +0300

    выполнил архив второго допа: 'Универсальная кнопка / combine стилей / children'

---
+++ archive-2/src/App.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import './App.css';
+import {Modal} from './components/modal/Modal';
+
+
+function App() {
+  return (
+    <div>
+      <Modal/>
+    </div>
+  );
+}
+
+
+export default App;
